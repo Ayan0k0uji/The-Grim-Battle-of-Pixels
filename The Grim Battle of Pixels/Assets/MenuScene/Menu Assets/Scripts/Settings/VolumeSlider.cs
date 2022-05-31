5a3d4b79d8fe190273fa508fdc7ca321b2da0960
artef00kt <artem_i02@mail.ru>
2022-06-01 00:04:44 +0400

    asd

--- The Grim Battle of Pixels/Assets/MenuScene/Menu Assets/Scripts/Settings/VolumeSlider.cs
+++ The Grim Battle of Pixels/Assets/MenuScene/Menu Assets/Scripts/Settings/VolumeSlider.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Audio;
 
-public class VolumeSlider : MonoBehaviour
+public class VolumeSlider : MonoBehaviour, IDeselectHandler, IUpdateSelectedHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    private float volume = 100f;
+    private string stringVolume;
+    [SerializeField] private AudioMixer am;
+    [SerializeField] private Text textValue;
+
+
+
+
+   void Start()
     {
-        
+        am.GetFloat("masterVolume", out volume);
+        //volume *= 100;
+        this.gameObject.GetComponent<Slider>().value = volume;
+        stringVolume = (((int)volume + 75) * 4 / 3).ToString() + " %";
+        textValue.text = stringVolume;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnUpdateSelected(BaseEventData eventData)
     {
-        
+        //if (textValue.text != "[ " + stringVolume + " ]")
+        //{
+            stringVolume = (((int)volume + 75)*4/3).ToString() + " %";
+            textValue.text = "[ " + stringVolume + " ]";
+       // }
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        textValue.text = stringVolume;
+    }
+
+    public void onChangeValue()
+    {
+        volume = this.gameObject.GetComponent<Slider>().value;
+        am.SetFloat("masterVolume", volume );
     }
 }
