9ca47cc8be887fd4d00a52f378caa9c00de82f24
Ayan0k0uji <101419149+Ayan0k0uji@users.noreply.github.com>
2022-06-02 12:03:04 +0400

    sry

--- The Grim Battle of Pixels/Assets/GameScene/Heroes/Babka/Snot/FlySnot.cs
+++ The Grim Battle of Pixels/Assets/GameScene/Heroes/Babka/Snot/FlySnot.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FlySnot : MonoBehaviour
 {
     private SpawnHeroes spawnHeroes;
-    private float speed = 3000f;
+    private float speed = 1000f;
     private Rigidbody2D _body;
     private GameObject enemy;
     private GameObject player;
@@ -13,6 +13,8 @@ public class FlySnot : MonoBehaviour
     private SpriteRenderer spriteRenderer;          // компонент спрайта слизи
     private Animator snotAnimator;
     private int damage = 1;                         // множитель урона слизи
+    private float napr;
+    private bool flag = true;
 
     private void Start()
     {
@@ -29,15 +31,26 @@ public class FlySnot : MonoBehaviour
 
         plStEnemy = enemy.GetComponent<PlayerStatus>();
         spriteRenderer = enemy.GetComponent<SpriteRenderer>();
-        Vector2 movement = Vector2.right * speed * Time.deltaTime * player.GetComponent<Transform>().localScale.x;
+        napr = player.GetComponent<Transform>().localScale.x;
+        Vector2 movement = Vector2.right * speed * Time.deltaTime * napr;
         _body.velocity = movement;
         gameObject.transform.parent = null;
+    }
+
+    private void FixedUpdate()
+    {
+        if (flag)
+        {
+            Vector2 movement = Vector2.right * speed * Time.deltaTime * napr;
+            _body.velocity = movement;
+        }
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.name == enemy.name && !collision.isTrigger)
         {
+            flag = false;
             plStEnemy.TakeDamage(40 / damage);
             enemy.GetComponent<PlayerStatus>().setSpeed(200);
             gameObject.GetComponent<CircleCollider2D>().enabled = false;
@@ -46,8 +59,8 @@ public class FlySnot : MonoBehaviour
         }
         else if (damage < 2 && collision.name != player.name && !collision.isTrigger)
         {
+            napr *= -1;
             damage += 1;
-            _body.velocity = -1 * _body.velocity;
             snotAnimator.SetBool("FlySnot", true);
         }
         else if (collision.name != player.name && !collision.isTrigger)
